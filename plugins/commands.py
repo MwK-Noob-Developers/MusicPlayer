da0a7fb4dc9efe550aaff031a1d63e8a429551cb
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-09-24 07:59:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,24 +1,4 @@
-#MIT License
 
-#Copyright (c) 2021 SUBIN
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import Client, filters
 from utils import USERNAME, mp
@@ -30,40 +10,22 @@ HOME_TEXT = "<b>Helo, [{}](tg://user?id={})\n\nIam MusicPlayer 2.0 which plays m
 HELP = """
 
 <b>
-Use /play <song name> or use /play as a reply to an audio file or youtube link.
-
-Use /yplay to play all the songs of a youtube playlist.
-
-You can also use <code>/splay song name</code> to play a song from Jio Saavn or <code>/splay -a album name</code> to play all the songs from a jiosaavn album or /cplay <channel username or channel id> to play music from a telegram channel.</b>
+Use /play <channel username> 
 
 **Common Commands**:
 
-**/play**  Reply to an audio file or YouTube link to play it or use /play <song name>.
-**/splay** Play music from Jio Saavn, Use /splay <song name> or <code>/splay -a album name</code> to play all the songs from that album.
-**/player**  Show current playing song.
 **/upload** Uploads current playing song as audio file.
 **/help** Show help for commands
 **/playlist** Shows the playlist.
 
 **Admin Commands**:
 **/skip** [n] ...  Skip current or n where n >= 2.
-**/cplay** Play music from a channel's music files.
-**/yplay** Play music from a youtube playlist.
-**/join**  Join voice chat.
-**/leave**  Leave current voice chat
 **/shuffle** Shuffle Playlist.
-**/vc**  Check which VC is joined.
-**/stop**  Stop playing.
 **/radio** Start Radio.
 **/stopradio** Stops Radio Stream.
 **/clearplaylist** Clear the playlist.
-**/export** Export current playlist for future use.
-**/import** Import a previously exported playlist.
-**/replay**  Play from the beginning.
-**/clean** Remove unused RAW PCM files.
 **/pause** Pause playing.
 **/resume** Resume playing.
-**/volume** Change volume(0-200).
 **/mute**  Mute in VC.
 **/unmute**  Unmute in VC.
 **/restart**  Update and restarts the Bot.
@@ -76,11 +38,11 @@ You can also use <code>/splay song name</code> to play a song from Jio Saavn or 
 async def start(client, message):
     buttons = [
         [
-        InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-        InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+        InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/mwkBoTs'),
+        InlineKeyboardButton('Developer', url='t.me/shamilnelli'),
     ],
     [
-        InlineKeyboardButton('👨🏼‍🦯 Help', callback_data='help'),
+        InlineKeyboardButton('How To Use Me', callback_data='help'),
         
     ]
     ]
@@ -95,8 +57,8 @@ async def start(client, message):
 async def show_help(client, message):
     buttons = [
         [
-        InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-        InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+        InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/mwkBoTs'),
+        InlineKeyboardButton('Developer', url='t.me/shamilnelli'),
     ]
     ]
     reply_markup = InlineKeyboardMarkup(buttons)
